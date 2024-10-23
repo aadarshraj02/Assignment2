b429e30ceac2fb6d985603c03fb0f9ffeca9ccb9
adarsh02 <aadarshraazz007@gmail.com>
2024-10-23 19:18:05 +0530

    added routes for CRUD in db

--- Assignment2/routes/routes.js
+++ Assignment2/routes/routes.js
@@ -1,11 +1,12 @@
-import express from "express";  //called express
-import games from "../data/data.js";   //called games data
+import express from "express"; //called express
+// import games from "../data/data.js";   //called games data
 
-const router = express.Router();  //called router
+import Game from "../gameSchema.js";
 
+const router = express.Router(); //called router
 
 //Middleware for validate game with required fields in post and put request
-const validateGame = (req, res, next) => {         
+const validateGame = (req, res, next) => {
   const { gameName, gameType, releaseYear } = req.body;
 
   if (!gameName || !gameType || !releaseYear) {
@@ -17,61 +18,74 @@ const validateGame = (req, res, next) => {
   next();
 };
 
-
 //Route to get all games
-router.get("/games", (req, res) => {
-  res.status(200).json(games);
-});
-
-//route to get game by specific id
-router.get("/game/:id", (req, res) => {
-  const game = games.find((g) => g.id === req.params.id);
-  if (game) {
-    res.status(200).json(game);
-  } else {
-    res.status(404).json({ message: "Game not found" });
+router.get("/games", async (req, res) => {
+  try {
+    const games = await Game.find();
+    res.status(200).json(games);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get all games" });
   }
 });
 
+//route to get game by specific id
+router.get("/games/:id", async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    if (game) {
+      res.status(200).json(game);
+    } else {
+      res.status(404).json({ message: "Game not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get games" });
+  }
+});
 
 //route to post game with validation of required fields
-router.post("/game", validateGame, (req, res) => {
+router.post("/game", validateGame, async (req, res) => {
   const { gameName, gameType, releaseYear } = req.body;
-  const newGame = {
-    id: String(games.length + 1),  //for new game id
-    gameName,
-    gameType,
-    releaseYear,
-  };
-  games.push(newGame); //pushing the newly added game into game data
-  res.status(201).json(newGame);
+
+  try {
+    const newGame = new Game({ gameName, gameType, releaseYear });
+    await newGame.save();
+    res.status(201).json(newGame);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to add new Game" });
+  }
 });
 
 //Updating game according to game id and validation
-router.put("/game/:id", validateGame, (req, res) => {
-  const game = games.find((g) => g.id === req.params.id);
-  if (game) {
-    game.gameName = req.body.gameName || game.gameName;
-    game.gameType = req.body.gameType || game.gameType;
-    game.releaseYear = req.body.releaseYear || game.releaseYear;
-    res.status(200).json(game);
-  } else {
-    res.status(404).json({ message: "Game not found" });
+router.put("/game/:id", validateGame, async (req, res) => {
+  const { gameName, gameType, releaseYear } = req.body;
+
+  try {
+    const updatedGame = await Game.findByIdAndUpdate(req.params.id, {
+      gameName,
+      gameType,
+      releaseYear,
+    });
+    if (updatedGame) {
+      res.status(200).json(updatedGame);
+    } else {
+      res.status(404).json({ message: "Game not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Failed to update game" });
   }
 });
 
 //route for deleting game according to id
-router.delete("/game/:id", (req, res) => {
-  const game = games.find((g) => g.id === req.params.id);
-
-  if (game) {
-    const updatedGames = games.filter((g) => g.id !== req.params.id);
-    games.length = 0;
-    games.push(...updatedGames);
-
-    res.status(200).json({ message: "Game deleted successfully" });
-  } else {
-    res.status(404).json({ message: "Game not found" });
+router.delete("/game/:id", async (req, res) => {
+  try {
+    const deletedGame = await Game.findByIdAndDelete(req.params.id);
+    if (deletedGame) {
+      res.status(200).json({ message: "Game deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Game not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete Game" });
   }
 });
 
